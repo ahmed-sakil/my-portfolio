d05602410a9c2587dd73de2e821a5a913fad0cc0
ahmed-sakil <thesakil.09@gmail.com>
2026-04-23 10:36:43 +0600

    Add click event listener to hero button

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 console.log("Portfolio loaded successfully!");
+
+document.getElementById("hello-btn").addEventListener("click", function() {
+    alert("Hello! Thanks for checking out my portfolio.");
+});
